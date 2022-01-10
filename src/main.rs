3a76c777a7b14838731ea89c3a2db51831e39269
AleBuser <alebuser.98@gmail.com>
2022-01-10 13:38:24 +0100

    add database migration macros

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-use local::api::server;
-use local::updater::update::update_db;
-
 use governor::{Quota, RateLimiter};
+use local::api::server;
+use local::storage::establish_connection;
+use local::updater::update::update_db;
 
 #[tokio::main]
 async fn main() {
+    // for github app
+    let pool = establish_connection().await;
+    sqlx::migrate!("./migrations").run(&pool).await.unwrap();
+
     // run the updater in the background
     let lim =
         RateLimiter::direct(Quota::with_period(std::time::Duration::from_secs(3600u64)).unwrap());
